07d8d02635fbfdf512964a081932e4c8149c674c
Zijin Zhang <58776897+Beforerr@users.noreply.github.com>
2025-06-18 09:41:17 -0700

    perf: do not materialize in `set_coefficients!` (#3)

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,20 @@
 using Documenter
 using GeoAACGM
 
-DocMeta.setdocmeta!(GeoAACGM, :DocTestSetup, :(using GeoAACGM; using PrettyPrinting); recursive=true)
+DocMeta.setdocmeta!(GeoAACGM, :DocTestSetup, :(using GeoAACGM; using PrettyPrinting); recursive = true)
 
 makedocs(
-    sitename="GeoAACGM.jl",
-    format=Documenter.HTML(),
-    modules=[GeoAACGM],
-    pages=[
+    sitename = "GeoAACGM.jl",
+    format = Documenter.HTML(),
+    modules = [GeoAACGM],
+    pages = [
         "Home" => "index.md",
     ],
-    checkdocs=:exports,
-    doctest=true
+    checkdocs = :exports,
+    doctest = true
 )
 
 deploydocs(
-    repo="github.com/Beforerr/GeoAACGM.jl",
+    repo = "github.com/Beforerr/GeoAACGM.jl",
+    push_preview = true
 )
