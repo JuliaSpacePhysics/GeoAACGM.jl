1f97b90fb3b65a8f903d8f4ea710bdf93cbec51c
Beforerr <zzj956959688@gmail.com>
2026-05-17 10:25:01 -0700

    refactor: simplify sh_contract by computing one component at a time

--- src/constants.jl
+++ src/constants.jl
@@ -1,5 +1,4 @@
 const RE = 6371.2  # Earth Radius in km
-const MAXALT = 2000.  # maximum altitude in km
 const NCOORD = 3  # xyz coordinates
 const POLYORD = 5  # quartic polynomial fit in altitude
 const SHORDER = 10  # order of Spherical Harmonic expansion
